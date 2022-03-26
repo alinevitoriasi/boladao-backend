5698276546ca39c108353da6c34e78ce1fbfda85
alinevitoriasi <alinevitoriabsi@gmail.com>
2022-03-26 00:36:13 -0300

    🐛 Remove bodyParser

--- config/express.js
+++ config/express.js
@@ -1,17 +1,15 @@
 const express = require('express');
 const consign = require('consign');
-const bodyParser = require('body-parser'); 
 const path = require('path');
-                    
-            
+                            
 module.exports = () => {
   const app = express();
   const cors = require("cors");
   app.use(cors());
   app.set('port', (5000));
 
-  app.use(bodyParser.urlencoded({extended: true}));
-  app.use(bodyParser.json());
+  app.use(express.urlencoded({extended: true}));
+  app.use(express.json());
 
   consign({cwd: 'server'})
     .include('models')
