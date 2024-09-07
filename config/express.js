99a6eac92f6c2f60ff1d40c1ef68257e4e541925
Aline <alinevitoriabsi@gmail.com>
2024-09-07 11:05:04 -0300

    Adiciona rate limiter

--- config/express.js
+++ config/express.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const consign = require('consign');
 const path = require('path');
-                            
+
+const rateLimit = require('express-rate-limit');
+
 module.exports = () => {
   const app = express();
   const cors = require("cors");
   const session = require('express-session');
-  
+
+  // Configuração do rate limiter
+  const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 15 minutos
+    max: 100, // limite de 100 requisições por IP por janela de tempo
+    message: 'Muitas requisições de seu IP, por favor tente novamente mais tarde.'
+  });
+  app.use(limiter);
+
   app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
   app.use(session({ secret:'@boladao-Token',saveUninitialized: false, resave: false }));
 
