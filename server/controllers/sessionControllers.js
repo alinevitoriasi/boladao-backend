471b9f53fab2ba2775e4fa80f064179f441ccf3c
Aline <alinevitoriabsi@gmail.com>
2024-09-08 15:25:09 -0300

    Removendo logs

--- server/controllers/sessionControllers.js
+++ server/controllers/sessionControllers.js
@@ -18,9 +18,9 @@ sessionController.login = async function (req, res, next) {
     }
 
     const token = jwt.sign({id: user._id},'@boladao-token',{expiresIn:"3d"})
+
     req.session.user = user;
-    req.session.save();
-    logger.info(`LOGIN TESTE: ${req}`);
+
     return res.status(200).json({auth:true, message:'TUDO SUPIMPA!', token:token, username:user?.username, isAdmin:user?.isAdmin })
   }
   catch(err) {
