40645694be3f4dc4520ff86c13ef8e8be861acb6
Aline <alinevitoriabsi@gmail.com>
2024-09-15 22:17:23 -0300

    Ajustando modelo post

--- server/models/post.js
+++ server/models/post.js
@@ -4,20 +4,23 @@ const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
   author:{
-    username : { 
+    username : {
       type: String,
       required: [true, 'Campo obrigatório']
     },
-    id : { 
+    id : {
       type: String,
       required: [true, 'Campo obrigatório']
     },
   },
+  location: String,
   text: {
     type: String,
     required: [true, 'Campo obrigatório!']
   },
   date: String,
+  type: [String],
+  isAnonymous: Boolean,
   comments:[{ text: String,  username: String, date: String}]
 });
 
