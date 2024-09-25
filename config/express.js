c65656b6bba7a79ca2d2d9251748350b86427cfc
Aline <alinevitoriabsi@gmail.com>
2024-09-25 19:31:55 -0300

    Adiciona configurações helmet

--- config/express.js
+++ config/express.js
@@ -11,16 +11,32 @@ const MongoStore = require('connect-mongo');
 module.exports = () => {
   const app = express();
 
-
   // Configuração do rate limiter
   const limiter = rateLimit({
     windowMs: 30 * 60 * 1000, // 15 minutos
-    max: 100, // limite de 100 requisições por IP por janela de tempo
+    max: 1000, // limite de 100 requisições por IP por janela de tempo
     message: 'Muitas requisições de seu IP, por favor tente novamente mais tarde.'
   });
   app.use(limiter);
 
-  app.use(helmet());
+  app.use(
+    helmet({
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "script-src": ["'self'"],
+          "img-src": ["'self'"],
+        },
+      },
+      crossOriginEmbedderPolicy: false,
+      crossOriginResourcePolicy: { policy: "same-origin" },
+      dnsPrefetchControl: { allow: false },
+      frameguard: { action: "deny" }, // Evita que seu site seja renderizado dentro de um iframe
+      hsts: { maxAge: 63072000, includeSubDomains: true, preload: true }, // HTTP Strict Transport Security
+      noSniff: true, // Evita a detecção MIME
+      xssFilter: true, // Proteção contra XSS
+    })
+  );
 
   const allowedOrigins = [
     'http://localhost:3000',
